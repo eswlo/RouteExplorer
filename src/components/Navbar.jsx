a820f6e681a42a5f09d7fd0adf20f7362e718fec
eswlo <sl3923@columbia.edu>
2024-09-09 14:07:20 -0700

    Add and style buttons in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,39 @@ export default function Navbar() {
         <nav>
             <h1 className="nav-title">Route Explorer</h1>
             <h3 className="nav-text">Dijkstra's algorithm</h3>
+            <fieldset className="nav-radio-field">
+                <input 
+                    type="radio"
+                    id="setStart"
+                    name="setState"
+                    value="setStart"
+                    // onChange={handle}
+                />
+                <label className="nav-radio-label" htmlFor="setStart">Set Start (by clicking a cell)</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="setEnd"
+                    name="setState"
+                    value="setEnd"
+                    // onChange={handle}
+                />
+                <label className="nav-radio-label" htmlFor="setEnd">Set End (by clicking a cell)</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="setBarriers"
+                    name="setState"
+                    value="setBarriers"
+                    // onChange={handle}
+                />
+                <label className="nav-radio-label" htmlFor="setBarriers">Set Barriers (by hovering over cells)</label>
+                <br />
+        
+            </fieldset>
+            <button className="nav-explore-btn">Explore</button>
         </nav>
     )
 }
